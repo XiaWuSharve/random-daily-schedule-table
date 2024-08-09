dec33f6ed1af0fdedb18b742af5b103372525b28
XiaWuSharve <sharve@foxmail.com>
2024-08-09 15:28:47 +0800

    Add an example for a table format Add beep for the remindation of every deadline.

--- lua.lua
+++ lua.lua
@@ -4,13 +4,13 @@ local function map(table, fun)
     local res = {}
     local p
     if type(table) == "table" then
-        p = function (t)
+        p = function(t)
             return pairs(t)
         end
     else
-        p = function (t)
+        p = function(t)
             local i = 0
-            return function ()
+            return function()
                 local v = t()
                 i = i + 1
                 if v then
@@ -28,7 +28,16 @@ end
 local function parseTime(str)
     local iter = string.gmatch(str, '(%d+)')
     local nums = map(iter, tonumber)
-    local date = {year=nums[1], month=nums[2], day=nums[3], hour=nums[4], min=nums[5]}
+    if #nums == 0 then
+        return nil
+    end
+    local date = {
+        hour = nums[1],
+        min = nums[2],
+        year = nums[3] or os.date('*t').year,
+        month = nums[4] or os.date('*t').month,
+        day = nums[5] or (os.date('*t').day + (nums[1] < 5 and 1 or 0))
+    }
     return os.time(date)
 end
 
@@ -45,36 +54,79 @@ local function loadData(filename, tableName)
         if not string.match(line, '(%s)') then
             break
         end
-        lines[#lines+1] = line
+        lines[#lines + 1] = line
     end
     return lines
 end
 
-local function genTable(outputPath)
+local function lookTableEndTime(time, units)
+    local iter = string.gmatch(time, '([^-]+)')
+    iter()
+    local endTime = iter()
+    return parseTime(endTime)
+end
+
+local function genTable(time, units)
+    local choosen = units[math.random(#units)]
+    io.write(time, ' ', choosen, '\n')
+end
+
+local function iterateTable(fun, ...)
     local filename = SKIN:ReplaceVariables(SKIN:GetVariable('TablePath'))
     local tableName = SKIN:GetVariable('TableName', 'default')
     local lines = loadData(filename, tableName)
 
-    local file = io.open(outputPath, "w+")
-    io.output(file)
+    local output = {}
+
     for _, line in pairs(lines) do
         local iter = string.gmatch(line, '([^%s]+)')
         local units = map(iter, tostring)
         local time = table.remove(units, 1)
-        local choosen = units[math.random(#units)]
-        io.write(time, ' ', choosen, '\n')
+        local ret = fun(time, units, ...)
+        if ret then
+            output[#output + 1] = ret
+        end
     end
-    io.close(file)
+    return output
 end
 
+local p = 1
+local endTimes
+
 function Initialize()
-    math.randomseed(os.time())
-    local lastUpdateTime = SKIN:GetVariable('LastUpdateTime', '1970-1-2 0:0')
+    local curTime = os.time()
+    math.randomseed(curTime)
+    local lastUpdateTime = SKIN:GetVariable('LastUpdateTime', '0:0 1970-1-2')
     local parsedLastUpdateTime = parseTime(lastUpdateTime)
-    local curDate = os.date('*t')
-    local threshhold = os.time({year=curDate['year'], month=curDate['month'], day=curDate['day'], hour=5})
+    local curDate = os.date('*t', curTime)
+    local threshhold = os.time({ year = curDate['year'], month = curDate['month'], day = curDate['day'], hour = 5 })
     if parsedLastUpdateTime < threshhold then
-        genTable(SKIN:ReplaceVariables(SKIN:GetVariable('OutputPath')))
-        SKIN:Bang('!WriteKeyValue', 'Variables', 'LastUpdateTime', os.date('%Y-%m-%d %H:%M'))
+        local outputPath = SKIN:ReplaceVariables(SKIN:GetVariable('OutputPath'))
+        local file = io.open(outputPath, "w+")
+        io.output(file)
+        iterateTable(genTable)
+        io.close(file)
+        SKIN:Bang('!WriteKeyValue', 'Variables', 'LastUpdateTime', os.date('%H:%M %Y-%m-%d'))
+    end
+    endTimes = iterateTable(lookTableEndTime)
+    p = 1
+    for i, v in ipairs(endTimes) do
+        if curTime < v then
+            p = i
+            break
+        end
+    end
+end
+
+function Update()
+    if p == #endTimes + 1 then
+        return
+    end
+    local script = [[
+        powershell -Command "for ($var = 1; $var -le 5; $var++) { [Console]::Beep() }"
+    ]]
+    if os.time() > endTimes[p] then
+        os.execute(script)
+        p = p + 1
     end
 end
